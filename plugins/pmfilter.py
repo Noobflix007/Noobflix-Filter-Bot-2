e6f0e7f57fa0b68f1c0c297c4a27005fc9ca867c
Surajsurya95096 <surajroxofficial01@gmail.com>
2025-09-05 02:43:50 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2026,14 +2026,14 @@ async def auto_filter(client, msg, spoll=False):
         )
         temp.IMDB_CAP[message.from_user.id] = cap
         if not settings.get('button'):
-            cap += "\n<b>♻️ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ 👇</b>"
+            cap += "<b>♻️ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ 👇</b>"
             for idx, file in enumerate(files, start=1):
                 cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>📕 {get_size(file.file_size)} | {clean_filename(file.file_name)}\n</a></b>"
     else:
         if settings.get('button'):
-            cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n📚 ᴀʟʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n⏰ ᴠɪᴇᴡ ʀᴇsᴜʟᴛ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n🙋‍♂ Hᴇʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or '@Deendayal_dhakadd'} \n♻️ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ 👇\n\n</b>"
+            cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n📚 ᴀʟʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n⏰ ᴠɪᴇᴡ ʀᴇsᴜʟᴛ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n🙋‍♂ Hᴇʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or '@Deendayal_dhakadd'} ♻️ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ 👇\n\n</b>"
         else:
-            cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n📚 ᴀʟʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n⏰ ᴠɪᴇᴡ ʀᴇsᴜʟᴛ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n🙋‍♂ Hᴇʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or '@Deendayal_dhakadd'} \n♻️ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ 👇\n\n</b>"
+            cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n📚 ᴀʟʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n⏰ ᴠɪᴇᴡ ʀᴇsᴜʟᴛ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n🙋‍♂ Hᴇʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or '@Deendayal_dhakadd'} ♻️ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ sᴇᴀʀᴄʜ 👇\n\n</b>"
 
             for idx, file in enumerate(files, start=1):
                 cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>📕 {get_size(file.file_size)} | {clean_filename(file.file_name)}\n</a></b>"
